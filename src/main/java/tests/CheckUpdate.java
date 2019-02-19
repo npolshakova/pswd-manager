30f6409156120fc4e7b8dd227809b428789f52d1
npolshakova <nina_polshakova@brown.edu>
2019-02-19 13:14:50 -0500

    fixed addr encoding

--- src/main/java/tests/CheckUpdate.java
+++ src/main/java/tests/CheckUpdate.java
@@ -16,7 +16,7 @@ public class CheckUpdate {
         NetworkParameters params = TestNet3Params.get();
         String filePrefix = "forwarding-service-testnet";
 
-        WalletAppKit kit = new WalletAppKit(params, new File("."), filePrefix) {
+        WalletAppKit kit = new WalletAppKit(params, new File(".."), filePrefix) {
             @Override
             protected void onSetupCompleted() {
                 if (wallet().getKeyChainGroupSize() < 1)
@@ -31,4 +31,8 @@ public class CheckUpdate {
 
     }
 
+    //https://testnet.blockchain.info/rawtx/a78dc9247981f9345a4a2431ad4b4f029a013cb5db9f44bbd8326b322d7297a9 WWWOOOO!
+
+    //ae50d064c5d8354680455b19ef2c559a64843287e71be956c4eeb0234c436479
+  // Received Reject: tx dfc0f6b28d5d659d07cf814a071730df2b229596b67d793b98ae2488033797fa for reason 'non-final'
 }
